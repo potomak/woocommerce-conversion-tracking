3d7bf1c507cb98e69a6f1dac7dfa85874ffac34f
Giovanni Cappellotto <potomak84@gmail.com>
2014-08-11 12:17:41 +0200

    Add FB tracking code

--- includes/integration.php
+++ includes/integration.php
@@ -9,22 +9,19 @@ class WeDevs_WC_Tracking_Integration extends WC_Integration {
 
     function __construct() {
 
-        $this->id = 'wc_conv_tracking';
-        $this->method_title = __( 'Conversion Tracking Pixel', 'wc-conversion-tracking' );
-        $this->method_description = __( 'Various conversion tracking pixel integration like Facebook Ad, Google Adwords, etc. Insert your scripts codes here:', 'wc-conversion-tracking' );
+        $this->id = 'wc_fb_pixel';
+        $this->method_title = __( 'FB Tracking Pixel', 'wc-fb-pixel' );
+        $this->method_description = __( 'Facebook conversion tracking pixel integration. Insert your pixel ids here:', 'wc-fb-pixel' );
 
         // Load the settings.
         $this->init_form_fields();
         $this->init_settings();
 
         add_action( 'woocommerce_update_options_integration', array($this, 'process_admin_options') );
-        add_action( 'woocommerce_product_options_reviews', array($this, 'product_options') );
-        add_action( 'woocommerce_process_product_meta', array($this, 'product_options_save'), 10, 2 );
 
-        add_action( 'woocommerce_registration_redirect', array($this, 'wc_redirect_url') );
-        add_action( 'template_redirect', array($this, 'track_registration') );
         add_action( 'wp_head', array($this, 'code_handler') );
-        add_action( 'wp_footer', array($this, 'code_handler') );
+        add_action( 'woocommerce_thankyou', array($this, 'checkout_tracking') );
+        add_action( 'woocommerce_after_main_content', array($this, 'single_product_tracking') );
     }
 
     /**
@@ -34,111 +31,93 @@ class WeDevs_WC_Tracking_Integration extends WC_Integration {
      */
     function init_form_fields() {
         $this->form_fields = array(
-            'position' => array(
-                'title' => __( 'Script Position', 'wc-conversion-tracking' ),
-                'description' => __( 'Select what position in your page you want to display the tag', 'wc-conversion-tracking' ),
+            'website' => array(
+                'title' => __( 'Website pixel id', 'wc-fb-pixel' ),
+                'description' => __( 'Pixel id to track page views', 'wc-fb-pixel' ),
                 'desc_tip' => true,
-                'id' => 'position',
+                'id' => 'website',
+                'type' => 'text',
+            ),
+            'currency' => array(
+                'title' => __( 'Currency', 'wc-fb-pixel' ),
+                'description' => __( 'Select event tracking currency', 'wc-fb-pixel' ),
+                'desc_tip' => true,
+                'id' => 'currency',
                 'type' => 'select',
                 'options' => array(
-                    'head' => __( 'Inside HEAD tag', 'wc-conversion-tracking' ),
-                    'footer' => __( 'In footer', 'wc-conversion-tracking' ),
+                    'SGD' => __( 'Singapore dollar', 'wc-fb-pixel' ),
+                    'RON' => __( 'Romanian new leu', 'wc-fb-pixel' ),
+                    'EUR' => __( 'Euro', 'wc-fb-pixel' ),
+                    'TRY' => __( 'Turkish lira', 'wc-fb-pixel' ),
+                    'SEK' => __( 'Swedish krona', 'wc-fb-pixel' ),
+                    'ZAR' => __( 'South African rand', 'wc-fb-pixel' ),
+                    'HKD' => __( 'Hong Kong dollar', 'wc-fb-pixel' ),
+                    'CHF' => __( 'Swiss franc', 'wc-fb-pixel' ),
+                    'NIO' => __( 'Nicaraguan córdoba', 'wc-fb-pixel' ),
+                    'JPY' => __( 'Japanese yen', 'wc-fb-pixel' ),
+                    'ISK' => __( 'Icelandic króna', 'wc-fb-pixel' ),
+                    'TWD' => __( 'New Taiwan dollar', 'wc-fb-pixel' ),
+                    'NZD' => __( 'New Zealand dollar', 'wc-fb-pixel' ),
+                    'CZK' => __( 'Czech koruna', 'wc-fb-pixel' ),
+                    'AUD' => __( 'Australian dollar', 'wc-fb-pixel' ),
+                    'THB' => __( 'Thai baht', 'wc-fb-pixel' ),
+                    'BOB' => __( 'Boliviano', 'wc-fb-pixel' ),
+                    'BRL' => __( 'Brazilian real', 'wc-fb-pixel' ),
+                    'MXN' => __( 'Mexican peso', 'wc-fb-pixel' ),
+                    'USD' => __( 'United States dollar', 'wc-fb-pixel' ),
+                    'ILS' => __( 'Israeli new shekel', 'wc-fb-pixel' ),
+                    'HNL' => __( 'Honduran lempira', 'wc-fb-pixel' ),
+                    'MOP' => __( 'Macanese pataca', 'wc-fb-pixel' ),
+                    'COP' => __( 'Colombian peso', 'wc-fb-pixel' ),
+                    'UYU' => __( 'Uruguayan peso', 'wc-fb-pixel' ),
+                    'CRC' => __( 'Costa Rican colon', 'wc-fb-pixel' ),
+                    'DKK' => __( 'Danish krone', 'wc-fb-pixel' ),
+                    'QAR' => __( 'Qatari riyal', 'wc-fb-pixel' ),
+                    'PYG' => __( 'Paraguayan guaraní', 'wc-fb-pixel' ),
+                    'CAD' => __( 'Canadian dollar', 'wc-fb-pixel' ),
+                    'INR' => __( 'Indian rupee', 'wc-fb-pixel' ),
+                    'KRW' => __( 'South Korean won', 'wc-fb-pixel' ),
+                    'GTQ' => __( 'Guatemalan quetzal', 'wc-fb-pixel' ),
+                    'AED' => __( 'United Arab Emirates dirham', 'wc-fb-pixel' ),
+                    'VEF' => __( 'Venezuelan bolívar fuerte', 'wc-fb-pixel' ),
+                    'SAR' => __( 'Saudi riyal', 'wc-fb-pixel' ),
+                    'NOK' => __( 'Norwegian krone', 'wc-fb-pixel' ),
+                    'CNY' => __( 'Chinese yuan', 'wc-fb-pixel' ),
+                    'ARS' => __( 'Argentine peso', 'wc-fb-pixel' ),
+                    'PLN' => __( 'Polish złoty', 'wc-fb-pixel' ),
+                    'GBP' => __( 'Pound sterling', 'wc-fb-pixel' ),
+                    'PEN' => __( 'Peruvian nuevo sol', 'wc-fb-pixel' ),
+                    'PHP' => __( 'Philippine peso', 'wc-fb-pixel' ),
+                    'VND' => __( 'Vietnamese dong', 'wc-fb-pixel' ),
+                    'RUB' => __( 'Russian rouble', 'wc-fb-pixel' ),
+                    'HUF' => __( 'Hungarian forint', 'wc-fb-pixel' ),
+                    'MYR' => __( 'Malaysian ringgit', 'wc-fb-pixel' ),
+                    'CLP' => __( 'Chilean peso', 'wc-fb-pixel' ),
+                    'IDR' => __( 'Indonesian rupiah', 'wc-fb-pixel' ),
                 )
             ),
-            'cart' => array(
-                'title' => __( 'Cart Scripts', 'wc-conversion-tracking' ),
-                'description' => __( 'Adds script on the cart page HEAD tag', 'wc-conversion-tracking' ),
-                'desc_tip' => true,
-                'id' => 'cart',
-                'type' => 'textarea',
-            ),
             'checkout' => array(
-                'title' => __( 'Checkout Scripts', 'wc-conversion-tracking' ),
-                'description' => __( 'Adds script on the purchase success page HEAD tag', 'wc-conversion-tracking' ),
+                'title' => __( 'Checkout pixel id', 'wc-fb-pixel' ),
+                'description' => __( 'Pixel id to track checkouts', 'wc-fb-pixel' ),
                 'desc_tip' => true,
                 'id' => 'checkout',
-                'type' => 'textarea',
+                'type' => 'text',
             ),
-            'reg' => array(
-                'title' => __( 'Registration Scripts', 'wc-conversion-tracking' ),
-                'description' => __( 'Adds script on the successful registraion page HEAD tag', 'wc-conversion-tracking' ),
+            'product' => array(
+                'title' => __( 'Product pixel id', 'wc-fb-pixel' ),
+                'description' => __( 'Pixel id to track product page views', 'wc-fb-pixel' ),
                 'desc_tip' => true,
-                'id' => 'registration',
-                'type' => 'textarea',
+                'id' => 'product',
+                'type' => 'text',
+            ),
+            'add-to-cart' => array(
+                'title' => __( 'Add to cart pixel id', 'wc-fb-pixel' ),
+                'description' => __( 'Pixel id to track add to cart actions', 'wc-fb-pixel' ),
+                'desc_tip' => true,
+                'id' => 'add-to-cart',
+                'type' => 'text',
             ),
         );
-    }
-
-    /**
-     * Save callback of Woo integration code settings API
-     *
-     * @param string $key
-     * @return string
-     */
-    function validate_textarea_field( $key ) {
-        $text = trim( stripslashes( $_POST[$this->plugin_id . $this->id . '_' . $key] ) );
-
-        return $text;
-    }
-
-    /**
-     * Saves tracking code of single product
-     *
-     * @uses woocommerce_process_product_meta
-     * @param int $post_id
-     */
-    function product_options_save( $post_id ) {
-
-        if ( isset( $_POST['_wc_conv_track'] ) ) {
-            $value = trim( $_POST['_wc_conv_track'] );
-            update_post_meta( $post_id, '_wc_conv_track', $value );
-        }
-    }
-
-    /**
-     * Textarea input box on product page
-     *
-     * Creates new textarea on WooCommerce product advanced tab for storing
-     * conversion tracking pixel for single product
-     *
-     * @uses woocommerce_product_options_reviews
-     */
-    function product_options() {
-
-        echo '<div class="options_group">';
-
-        woocommerce_wp_textarea_input( array('id' => '_wc_conv_track', 'label' => __( 'Conversion Tracking Code', 'wc-conversion-tracking' ), 'desc_tip' => 'true', 'description' => __( 'Insert conversion tracking code for this product.', 'wc-conversion-tracking' )) );
-
-        echo '</div>';
-    }
-
-    /**
-     * Print registration code when particular GET tag exists
-     *
-     * @uses add_action()
-     * @return void
-     */
-    function track_registration() {
-        if ( isset( $_GET['_wc_user_reg'] ) && $_GET['_wc_user_reg'] == 'true' ) {
-            add_action( 'wp_head', array($this, 'print_reg_code') );
-        }
-    }
-
-    /**
-     * Adds a url query arg to determine newly registered user
-     *
-     * @uses woocommerce_registration_redirect action
-     * @param string $redirect
-     * @return string
-     */
-    function wc_redirect_url( $redirect ) {
-        if ( wp_get_referer() ) {
-            $redirect = add_query_arg( array('_wc_user_reg' => 'true'), esc_url( wp_get_referer() ) );
-        } else {
-            $redirect = add_query_arg( array('_wc_user_reg' => 'true'), esc_url( get_permalink( wc_get_page_id( 'myaccount' ) ) ) );
-        }
-
-        return $redirect;
     }
 
     /**
@@ -151,53 +130,81 @@ class WeDevs_WC_Tracking_Integration extends WC_Integration {
      * @return void
      */
     function code_handler() {
+        echo "<!-- Tracking pixel by WooCommerce FB Pixel plugin -->\n";
+        echo <<<EOSTR
+<script>(function() {
+  var _fbq = window._fbq || (window._fbq = []);
+  if (!_fbq.loaded) {
+    var fbds = document.createElement('script');
+    fbds.async = true;
+    fbds.src = '//connect.facebook.net/en_US/fbds.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(fbds, s);
+    _fbq.loaded = true;
+  }
+  _fbq.push(['addPixelId', '{$this->get_option( 'website' )}']);
+})();
+window._fbq = window._fbq || [];
+window._fbq.push(['track', 'PixelInitialized', {}]);
+</script>
+<noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?id={$this->get_option( 'website' )}&amp;ev=NoScript" /></noscript>
+EOSTR;
+        echo "\n<!-- Tracking pixel by WooCommerce FB Pixel plugin -->\n";
+    }
 
-        $position = $this->get_option( 'position', 'head' );
+    function checkout_tracking() {
+        global $order;
 
-        if ( 'wp_head' == current_filter() && $position != 'head' ) {
-            return;
-        } elseif ( 'wp_footer' == current_filter() && $position != 'footer' ) {
-            return;
-        }
-
-        if ( is_cart() ) {
-
-            echo $this->print_conversion_code( $this->get_option( 'cart' ) );
-
-        } elseif ( is_order_received_page() ) {
-
-            echo $this->print_conversion_code( $this->get_option( 'checkout' ) );
-
-        } elseif ( is_product() ) {
-
-            $code = get_post_meta( get_the_ID(), '_wc_conv_track', true );
-            echo $this->print_conversion_code( $code );
+        if ( !in_array( $order->status, array( 'failed' ) ) ) {
+            echo "<!-- Tracking pixel by WooCommerce FB Pixel plugin -->\n";
+            $code = <<<EOSTR
+<script>
+window._fbq.push(['track', '{$this->get_option( 'checkout' )}', {'value':'{$order->get_total()}','currency':'{$this->get_option( 'currency' )}'}]);
+</script>
+<noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?ev={$this->get_option( 'checkout' )}&amp;cd[value]={$order->get_total()}&amp;cd[currency]={$this->get_option( 'currency' )}&amp;noscript=1" /></noscript>
+EOSTR;
+            echo "\n<!-- Tracking pixel by WooCommerce FB Pixel plugin -->\n";
         }
     }
 
-    /**
-     * Registration code print handler
-     *
-     * @return void
-     */
-    function print_reg_code() {
-        echo $this->print_conversion_code( $this->get_option( 'reg' ) );
-    }
+    function single_product_tracking() {
+        global $product;
 
-    /**
-     * Prints the code
-     *
-     * @param string $code
-     * @return void
-     */
-    function print_conversion_code( $code ) {
-        if ( $code == '' ) {
-            return;
+        if ( is_product() ) {
+            echo "<!-- Tracking pixel by WooCommerce FB Pixel plugin -->\n";
+            echo <<<EOSTR
+<script>
+window._fbq.push(['track', '{$this->get_option( 'product' )}', {'value':'{$product->get_price()}','currency':'{$this->get_option( 'currency' )}','sku':'{$product->get_sku()}'}]);
+
+try {
+  jQuery(document).ready(function() {
+    jQuery('form.cart').submit(function() {
+      try {
+        var quantity = parseInt(jQuery('form.cart input[name=quantity]').val(), 10);
+        var variation_id = jQuery('form.cart input[name=variation_id]').val();
+        var variations_found = jQuery('form.cart').data('product_variations').filter(function(variation) {
+          return variation.variation_id == variation_id;
+        });
+        if (variations_found.length > 0) {
+          var price_match = variations_found[0].price_html.match(/\d+.\d+/);
+          if (price_match.length > 0) {
+            var price = parseFloat(price_match[0]);
+            window._fbq.push(['track', '{$this->get_option( 'add-to-cart' )}', {'value':quantity*price,'currency':'{$this->get_option( 'currency' )}','variation_id':variation_id}]);
+          }
         }
-
-        echo "<!-- Tracking pixel by WooCommerce Conversion Tracking plugin -->\n";
-        echo $code;
-        echo "\n<!-- Tracking pixel by WooCommerce Conversion Tracking plugin -->\n";
+      } catch(e) {
+        window._fbq.push(['track', '{$this->get_option( 'add-to-cart' )}', {'value':'{$product->get_price()}','currency':'{$this->get_option( 'currency' )}'}]);
+      }
+    });
+  });
+} catch(e) {
+  // NOOP
+}
+</script>
+<noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?ev={$this->get_option( 'product' )}&amp;cd[value]={$product->get_price()}&amp;cd[currency]={$this->get_option( 'currency' )}&amp;cd[sku]={$product->get_sku()}&amp;noscript=1" /></noscript>
+EOSTR;
+            echo "\n<!-- Tracking pixel by WooCommerce FB Pixel plugin -->\n";
+        }
     }
 
 }
